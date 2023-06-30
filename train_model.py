fb781cc223e08ead513faafea6526b41fa6dbcf4
Daniele Silvestro <silvestro.daniele@gmail.com>
2023-06-30 11:19:48 +0200

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -10,18 +10,20 @@ sim, dict_inputs, dict_outputs = pn.rnn_in_out_dictionaries_from_sim(training_fi
                                                                      log_rates=False,
                                                                      log_tree_len=True,
                                                                      output_list=['per_site_rate','tree_len'],
-                                                                     include_tree_features=False,)
+                                                                     include_tree_features=False)
 
 # setup model architecture
 model_config = pn.rnn_config(n_sites=1000, n_taxa=50) # default settings
 
 # build model
-model = pn.build_rnn_model(model_config, optimizer=pn.keras.optimizers.RMSprop(1e-3))
-# model.summary()
+model = pn.build_rnn_model(model_config,
+                           optimizer=pn.keras.optimizers.RMSprop(1e-3),
+                           print_summary=False)
+
 
 # training
 early_stop = pn.keras.callbacks.EarlyStopping(monitor="val_loss",
-                                              patience=20,
+                                              patience=5,
                                               restore_best_weights=True)
 
 history = model.fit(dict_inputs, dict_outputs,
@@ -32,7 +34,5 @@ history = model.fit(dict_inputs, dict_outputs,
                     batch_size=100)
 
 # save model
-pn.save_rnn_model(wd=wd,
-                  history=history,
-                  model=model, feature_rescaler=None, filename=model_name)
+pn.save_rnn_model(wd=wd, history=history, model=model, filename=model_name)
 
