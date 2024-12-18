c7b1139b5a69b9ca1048b2cf7289efb5fa6cb169
F4llis <clement.train@gmail.com>
2024-12-18 23:29:13 +0100

    Makes phylornn as pip, can worked outside the folder, remove toplevel conflict in case you have data or logs folder in the package folder, restructure

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name="phyloRNN",
+    version="0.41.1",
+    author="Davide Silvestro, Thibault Latrille",
+    author_email="davide.silvestro@unil.ch, thibault.latrille@unil.ch",
+    description="A project for phylogenetic analysis using RNNs",
+    url="https://github.com/phyloRNN/phyloRNN",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.8',
+    install_requires=requirements,
+)
